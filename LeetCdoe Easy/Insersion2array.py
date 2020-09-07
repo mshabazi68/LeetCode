252a15abec6c31e914b1457758cde52ee1ecf73f
Mohammadreza Shahbazi <mshabazi68@gmail.com>
2020-09-06 23:30:59 -0700

    Add files via upload

---
+++ LeetCdoe Easy/Insersion2array.py
@@ -0,0 +1,23 @@
+class Insertion2array:
+    def intersect(self, nums1, nums2):
+
+        nums1.sort()
+        nums2.sort()
+
+        p1 = p2 = 0
+        result = []
+
+        while p1 < len(nums1) and p2 < len(nums2):
+            num1 = nums1[p1]
+            num2 = nums2[p2]
+
+            if num1 < num2:
+                p1 += 1
+            elif num1 > num2:
+                p2 += 1
+            else:
+                result.append(num1)
+                p1 += 1
+                p2 += 1
+
+        return result
