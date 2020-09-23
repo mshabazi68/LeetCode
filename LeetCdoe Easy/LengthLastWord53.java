35202d54313f8921307fa588f68981afe363bee5
Mohammadreza Shahbazi <mshabazi68@gmail.com>
2020-09-22 23:51:21 -0700

    Add files via upload

---
+++ LeetCdoe Easy/LengthLastWord53.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class LengthLastWord53 {
+
+	public int lengthOfLastWord(String s) { 
+	    if(s==null || s.length() == 0)
+	        return 0;
+	 
+	    int result = 0;
+	    int len = s.length();
+	 
+	    boolean flag = false;
+	    for(int i=len-1; i>=0; i--){
+	        char c = s.charAt(i);
+	        if((c>='a' && c<='z') || (c>='A' && c<='Z')){
+	            flag = true;
+	            result++;
+	        }else{
+	            if(flag)
+	                return result;
+	        }
+	    }
+	 
+	    return result;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
