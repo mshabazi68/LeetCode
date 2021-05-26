59f757b6bae86907784081bfc331e5869048a33f
Reza Shahbazi <mshabazi68@gmail.com>
2021-05-25 23:32:42 -0700

    Delete Node in a Linked List

---
+++ LeetCdoe Easy/DeleteNodeLinklist.cs
@@ -0,0 +1,26 @@
+// 237  Delete Node in a Linked List 
+//Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+//It is guaranteed that the node to be deleted is not a tail node in the list.
+/* Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function. */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public void DeleteNode(ListNode node) {
+       
+        if(node == null)
+            return;
+        
+        node.val = node.next.val; 
+        
+        node.next = node.next.next; 
+    }
+}
