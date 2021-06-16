c0d8f2c27e0abb4ab43e87b4b3af23b321b2c1f7
Reza Shahbazi <mshabazi68@gmail.com>
2021-06-15 21:37:59 -0700

    Create DivideTwoInt.java

---
+++ LeetCode Medium/DivideTwoInt.java
@@ -0,0 +1,42 @@
+/*
+Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, assume that your function returns 231 − 1 when the division result overflows.
+
+
+*/
+
+public class DivideTwoIntegers {
+
+    public int divide(int dividend, int divisor) {
+        // Check for overflow
+        if (divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1)) {
+            return Integer.MAX_VALUE;
+        }
+        // Sign of result
+        int sign = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? -1 : 1;
+        // Quotient
+        int quotient = 0;
+        // Take the absolute value
+        long absoluteDividend = Math.abs((long) dividend);
+        long absoluteDivisor = Math.abs((long) divisor);
+        // Loop until the  dividend is greater than divisor
+        while (absoluteDividend >= absoluteDivisor) {
+            // This represents the number of bits shifted or
+            // how many times we can double the number
+            int shift = 0;
+            while (absoluteDividend >= (absoluteDivisor << shift)) {
+                shift++;
+            }
+            // Add the number of times we shifted to the quotient
+            quotient += (1 << (shift - 1));
+            // Update the dividend for the next iteration
+            absoluteDividend -= absoluteDivisor << (shift - 1);
+        }
+        return sign == -1 ? -quotient : quotient;
+    }
+}
