1794932dbb10679631f7b0a7563430d1f8310206
Reza Shahbazi <mshabazi68@gmail.com>
2021-08-02 17:19:59 -0700

    Create Delete NodeInLinkedList.java

---
+++ LeetCdoe Easy/Delete NodeInLinkedList.java
@@ -0,0 +1,18 @@
+/**
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val; 
+        node.next = node.next.next;
+        
+    }
+}
