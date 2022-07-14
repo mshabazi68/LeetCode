ec7040ab4bdbdecc1a3716175abdc70870cfd543
Reza Shahbazi <mshabazi68@gmail.com>
2022-07-14 00:01:03 -0700

    Update PlusOne66.java

--- LeetCodeEasy/PlusOne66.java
+++ LeetCodeEasy/PlusOne66.java
@@ -1,42 +1,27 @@
-  import java.util.Arrays;
+import java.util.Arrays;
 
 public class PlusOne66 {
 	public int[] plusOne(int[] digits) {
-		String num ="";
-		for (int i=0; i<digits.length; i++) {
-			 num = num + Integer.toString(digits[i]);
-		}
-		
-		int temp = Integer.parseInt(num) +1;
-		int count = 0;
-		int n = temp;
-		while (n != 0)
-		{
-		    n /= 10;
-		    count++;
-		}
-		int numberArray[] = new int [count];
-		if(count!=0){
-			   
-
-			   count = 0;    
-			   n = temp;
-
-			   while (n != 0){
-			       numberArray[count] = n % 10;
-			       n /= 10;
-			       count++;
-			   }
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i] = digits[i] + 1;
+				return digits;
 			}
-		System.out.println(Arrays.toString(numberArray));
-		return numberArray;
+			digits[i] = 0;
+			//System.out.println(Arrays.toString(digits));
+		}
+		int result[] = new int[digits.length + 1];
+		result[0] = 1;
+		//System.out.println(Arrays.toString(result));
+		return result;
+
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		PlusOne66 obj = new PlusOne66();
-		int [] digits = {1,2,3};
-		System.out.println(obj.plusOne(digits));
+		int[] digits = { 1, 2, 9 };
+		System.out.println(Arrays.toString(obj.plusOne(digits)));
 	}
 
 }
