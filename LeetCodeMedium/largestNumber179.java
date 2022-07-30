b111cb8cd01046399969835062aa85ca15f98517
Reza Shahbazi <mshabazi68@gmail.com>
2022-07-29 23:03:50 -0700

    Create largestNumber179.java

---
+++ LeetCodeMedium/largestNumber179.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public String largestNumber(int[] nums) {
+        Integer[] ints = new Integer[nums.length];
+        int i=0;
+        for(int v : nums){
+            ints[i++] =v;
+        }
+        Comparator<Integer> comp = new Comparator<Integer>(){
+            @Override
+            public int compare(Integer s1, Integer s2){
+                String c1 = Integer.toString(s1) + Integer.toString(s2);
+                String c2 = Integer.toString(s2) + Integer.toString(s1);
+
+                return 0 - c1.compareTo(c2);
+            }
+        };
+
+        Arrays.sort(ints, comp);
+
+        if(ints[0] == 0) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for(int v : ints){
+            sb.append(v);
+        }
+
+        return sb.toString();
+    }
+}
